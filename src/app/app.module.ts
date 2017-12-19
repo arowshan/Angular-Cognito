0002f9bed4fdc0b9478f7364773e413d2b67b240
Rowshan <rowshana2@nih.gov>
2017-12-19 11:22:47 -0500

    login implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { RegisterComponent } from './register/register.component';
 import { CognitoUtil } from './register/cognito.service';
 import { ConfirmRegistrationComponent } from './confirm-registration/confirm-registration.component';
 import { RegisterService } from './register/register.service';
+import { LoginComponent } from './login/login.component';
+import { LoginService } from './login/login.service';
 
 @NgModule({
-  declarations: [AppComponent, RegisterComponent, ConfirmRegistrationComponent],
+  declarations: [AppComponent, RegisterComponent, ConfirmRegistrationComponent, LoginComponent],
   imports: [BrowserModule, FormsModule],
-  providers: [CognitoUtil, RegisterService],
+  providers: [CognitoUtil, RegisterService, LoginService],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/app/login/login.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@angular/core';
+import { AuthenticationDetails, CognitoUser } from 'amazon-cognito-identity-js';
+import { CognitoUtil } from '../register/cognito.service';
+import { STS } from 'aws-sdk';
+import { environment } from '../../environments/environment';
+import * as AWS from 'aws-sdk';
+
+@Injectable()
+export class LoginService {
+
+  constructor(public cognitoUtil: CognitoUtil) { }
+
+  authenticate(username: string, password: string) {
+
+    let authenticationData = {
+      Username: username,
+      Password: password,
+    };
+    let authenticationDetails = new AuthenticationDetails(authenticationData);
+
+    let userData = {
+      Username: username,
+      Pool: this.cognitoUtil.getUserPool()
+    };
+
+    let cognitoUser = new CognitoUser(userData);
+
+    var self = this;
+    cognitoUser.authenticateUser(authenticationDetails, {
+      newPasswordRequired: function (userAttributes, requiredAttributes) {
+        // callback.cognitoCallback(`User needs to set password.`, null);
+      },
+      onSuccess: function (result) {
+
+        let creds = self.cognitoUtil.buildCognitoCreds(result.getIdToken().getJwtToken());
+
+        AWS.config.credentials = creds;
+
+        // So, when CognitoIdentity authenticates a user, it doesn't actually hand us the IdentityID,
+        // used by many of our other handlers. This is handled by some sly underhanded calls to AWS Cognito
+        // API's by the SDK itself, automatically when the first AWS SDK request is made that requires our
+        // security credentials. The identity is then injected directly into the credentials object.
+        // If the first SDK call we make wants to use our IdentityID, we have a
+        // chicken and egg problem on our hands. We resolve this problem by "priming" the AWS SDK by calling a
+        // very innocuous API call that forces this behavior.
+        let clientParams: any = {};
+        if (environment.sts_endpoint) {
+          clientParams.endpoint = environment.sts_endpoint;
+        }
+        let sts = new STS(clientParams);
+        sts.getCallerIdentity(function (err, data) {
+          console.log("UserLoginService: Successfully set the AWS credentials");
+          // callback.cognitoCallback(null, result);
+        });
+
+      },
+      onFailure: function (err) {
+        // callback.cognitoCallback(err.message, null);
+      },
+    });
+  }
+
+}
